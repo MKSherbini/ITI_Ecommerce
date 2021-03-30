a80045dce3eaff70db18325ca238fa16a3474771
MKSherbini <mksherbini@gmail.com>
2021-03-30 23:28:25 +0200

    ground work

---
+++ Website/src/main/java/controllers/card/CardEditController.java
@@ -0,0 +1,40 @@
+package controllers.card;
+
+import constants.UrlMappingConstants;
+import constants.enums.PageNames;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import listeners.ThreadLocalContext;
+import models.dtos.CartDto;
+
+import java.io.IOException;
+
+@WebServlet("/cardEdit")
+public class CardEditController extends HttpServlet {
+
+    ServletConfig myConfig;
+
+    public void init(ServletConfig config) throws ServletException {
+        myConfig = config;
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ThreadLocalContext.includeView(PageNames.CARD_EDIT);
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
